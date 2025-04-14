e3f66ac2e17a621aeb999c07fe510e60ef8b4d0c
Mirabelle Morah <myrakuti@gmail.com>
2025-04-14 20:24:32 +0100

    update mobile header

--- menu.js
+++ menu.js
@@ -1,9 +1,11 @@
 const toggleTag = document.querySelector("a.toggle-nav");
 const mainTag = document.querySelector("main");
+const HTag = document.querySelector("header nav");
 
 toggleTag.addEventListener("click", function (event) {
     event.preventDefault(); // 👈 prevents that annoying scroll-to-top
     mainTag.classList.toggle("open");
+      HTag.classList.toggle("open");
 
     if (mainTag.classList.contains("open")) {
         toggleTag.innerHTML = `<img src="./assets/home-page-assets/close.svg"> Close`;
@@ -11,6 +13,5 @@ toggleTag.addEventListener("click", function (event) {
     else {
         toggleTag.innerHTML = `<img src="./assets/home-page-assets/menu.svg"> Menu`;
     }
-
 
 });
