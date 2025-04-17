247a8ce0bfb70e2a7929643a2d855fed2ceb1587
Mirabelle Morah <myrakuti@gmail.com>
2025-04-17 07:55:11 +0100

    changes

--- menu.js
+++ menu.js
@@ -1,11 +1,10 @@
 const toggleTag = document.querySelector("a.toggle-nav");
 const mainTag = document.querySelector("main");
-const HTag = document.querySelector("header nav");
+
 
 toggleTag.addEventListener("click", function (event) {
     event.preventDefault(); // 👈 prevents that annoying scroll-to-top
     mainTag.classList.toggle("open");
-      HTag.classList.toggle("open");
 
     if (mainTag.classList.contains("open")) {
         toggleTag.innerHTML = `<img src="./assets/home-page-assets/close.svg"> Close`;
